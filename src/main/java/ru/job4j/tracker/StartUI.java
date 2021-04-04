e7f58d089e379465b7d1d7ccdea061d8630313d1
JuliaSleptsova <yuli.sleptsova@gmail.com>
2021-04-04 08:47:51 +0300

    9. toString [#492505]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,13 +5,13 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Item item = new Item();
+        Item item = new Item("AN", 1);
         LocalDateTime currentDateTime = item.getCreated();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
         String currentDateTimeFormat = currentDateTime.format(formatter);
         System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
         Tracker tracker = new Tracker();
-        tracker.add(new Item("AN"));
-        System.out.println(tracker.findById(1));
+        tracker.add(item);
+        System.out.println(tracker.findById(item.getId()));
     }
 }
